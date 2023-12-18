fe6aeb190eb78cda6ed0a609b6813e8a0b4e508e
늅이 <noobnuby77@gmail.com>
2023-12-18 13:13:35 +0900

    add pause window start.bat
    윈도우 실행 배치파일에 pause가 없는거 수정

--- src/main/java/kr/vanilage/FileSetting.java
+++ src/main/java/kr/vanilage/FileSetting.java
@@ -38,6 +38,7 @@ public class FileSetting {
 
             pw.printf("java -Xms%dM -Xmx%dM --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -jar server.jar", memory, memory);
             if (nogui) pw.print(" nogui");
+            pw.print("\npause")
 
             pw.close();
         }
